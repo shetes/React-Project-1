f4db8f78dcfc26637997c437edb5a238558283b5
Shailendra Shete <shetes@gmail.com>
2016-03-18 17:50:13 -0400

    Added JSX components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
-'use strict';
-
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log("Message from main.js.");
+(function(win) {
+    "use strict";
+    
+    var App = React.createClass({
+        render: function() {
+            var Child;
+            
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+            
+            return (
+                <div>
+                    <Child/>
+                </div>
+            );
+        }
+    });
 
-module.exports = App;
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+    
+})(window);
+
+
